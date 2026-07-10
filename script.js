691dcef77f6502b2585529195cd796d2cfd9c2db
Brian C <283823263+reisntea@users.noreply.github.com>
2026-07-09 23:15:52 -0400

    Add function to display books and event on hover
    Added a function that displays all the books on the shelf. Also added an
    event on mouseover that displays the info in the box.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const bookshelf = document.querySelector("#books");
+const infoText = document.querySelector("#info-text");
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -26,12 +28,48 @@ function addBookToLibrary (title, author, pages, read) {
 
 addBookToLibrary("waldo", "some guy", 290, false);
 addBookToLibrary("waldy", "some guy", 20, true);
+addBookToLibrary("Of Mice and Men", "some guy", 400, false);
 
-console.log(myLibrary[0]);
-console.log(myLibrary[0].id);
-console.log(myLibrary[1]);
-console.log(myLibrary[1].id);
+updateLibraryDisplay();
 
 function updateLibraryDisplay () {
-    
+    let bookWidth = 0;
+    for (Book of myLibrary) {
+        const book = document.createElement("div");
+        const bookTitle = document.createElement("p");
+
+        if (Book.pages / 100 < 1.5) {
+            bookWidth = 1.5;
+        } else if (Book.pages / 100 > 7) {
+            bookWidth = 7;
+        } else {
+            bookWidth = Book.pages / 100;
+        }
+        
+
+        book.setAttribute("class", "book");
+        book.style.minWidth = `${bookWidth}%`;
+        book.style.height = Math.floor(Math.random() * (100 - 60) + 55) + "%";
+        book.setAttribute("data-id", `${Book.id}`);
+
+        bookTitle.textContent = Book.title;
+
+        book.appendChild(bookTitle);
+        bookshelf.appendChild(book);
+    }
 }
+
+let currentId = 0;
+let text = "";
+
+document.querySelector('#books').addEventListener('mouseover', (event) => {
+  if (event.target.matches('.book') && currentId != event.target.getAttribute('data-id')) {
+    console.log('Element hovered via delegation:', event.target.getAttribute('data-id'));
+
+    currentId = event.target.getAttribute('data-id');
+    text = myLibrary.find(({id}) => id === event.target.getAttribute('data-id')).info();
+    console.log(text);
+
+    infoText.textContent = text;
+  }
+});
