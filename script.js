076ac259c5a777f27ece4ad189a6de96bfc2a19e
Brian C <283823263+reisntea@users.noreply.github.com>
2026-07-10 20:12:54 -0400

    Add ability to add books, fixed many bugs
    Books can now be added by pressing the add book button which opens a
    dialog with a form on it. There was also a bug with updating the
    bookshelf because I made the loop alter the objects themselves
    accidentally. Other minor bugs were also fixed.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const myLibrary = [];
 const bookshelf = document.querySelector("#books");
+
 const infoContent = document.querySelector("#info-contents");
 const infoText = document.querySelector("#info-text");
+
+const submitButton = document.querySelector("#submit-button");
+const bookDialog = document.querySelector("#book-dialog");
+const bookForm = document.querySelector("#add-book");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
+
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -23,68 +33,53 @@ Book.prototype.info = function() {
         return (this.read) ? `${this.title} by ${this.author}, ${this.pages} pages, has been read` : `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
 }
 
-function addBookToLibrary (title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
+Book.prototype.changeReadStatus = function() {
+    this.read ? this.read = false : this.read = true;
+    updateInfoDisplay(this);
 }
 
-function changeReadStatus (Book) {
-    Book.read ? Book.read = false : Book.read = true;
-    updateInfoDisplay(Book);
-}
-
-function removeBook (Book) {
-    myLibrary.splice(myLibrary.indexOf(Book), 1);
+Book.prototype.removeBook = function() {
+    myLibrary.splice(myLibrary.indexOf(this), 1);
     infoText.textContent = "";
     infoContent.querySelectorAll('button').forEach(button => button.remove());
     updateLibraryDisplay();
 }
 
-addBookToLibrary("waldo", "some guy", 290, false);
-addBookToLibrary("waldy", "some guy", 20, true);
-addBookToLibrary("Of Mice and Men", "some guy", 400, false);
-
-updateLibraryDisplay();
+function addBookToLibrary (title, author, pages, read) {
+    console.log(typeof Book);
+    myLibrary.push(new Book(title, author, pages, read));
+}
 
 function updateLibraryDisplay () {
     bookshelf.replaceChildren();
     let bookWidth = 0;
-    for (Book of myLibrary) {
-        const book = document.createElement("div");
+    for (const book of myLibrary) {
+        const bookBox = document.createElement("div");
         const bookTitle = document.createElement("p");
 
-        if (Book.pages / 100 < 1.5) {
+        if (book.pages / 100 < 1.5) {
             bookWidth = 1.5;
-        } else if (Book.pages / 100 > 7) {
+        } else if (book.pages / 100 > 7) {
             bookWidth = 7;
         } else {
-            bookWidth = Book.pages / 100;
+            bookWidth = book.pages / 100;
         }
         
 
-        book.setAttribute("class", "book");
-        book.style.minWidth = `${bookWidth}%`;
-        book.setAttribute("data-id", `${Book.id}`);
+        bookBox.setAttribute("class", "book");
+        bookBox.style.minWidth = `${bookWidth}%`;
+        bookBox.setAttribute("data-id", `${book.id}`);
 
-        bookTitle.textContent = Book.title;
+        bookTitle.textContent = book.title;
 
-        book.appendChild(bookTitle);
-        bookshelf.appendChild(book);
+        bookBox.appendChild(bookTitle);
+        bookshelf.appendChild(bookBox);
     }
 }
 
-let currentId = 0;
-let text = "";
-
-document.querySelector('#books').addEventListener('mouseover', (event) => {
-    if (event.target.matches('.book') && currentId != event.target.getAttribute('data-id')) {
-        currentId = event.target.getAttribute('data-id');
-        updateInfoDisplay(myLibrary.find(({id}) => id === event.target.getAttribute('data-id')));
-    }
-});
-
 function updateInfoDisplay (Book) {
     infoContent.querySelectorAll('button').forEach(button => button.remove());
-    text = Book.info();
+    const text = Book.info();
     const readButton = document.createElement("button");
     const removeButton = document.createElement("button");
 
@@ -101,17 +96,52 @@ function updateInfoDisplay (Book) {
     infoContent.appendChild(removeButton);
 }
 
+let currentId = 0;
+
+document.querySelector('#books').addEventListener('mouseover', (event) => {
+    if (event.target.matches('.book') && currentId != event.target.getAttribute('data-id')) {
+        currentId = event.target.getAttribute('data-id');
+        updateInfoDisplay(myLibrary.find(({id}) => id === event.target.getAttribute('data-id')));
+    }
+});
+
 infoContent.addEventListener('click', (event) => {
     let target = event.target;
 
     switch(target.id) {
         case 'change-read':
             console.log('change read item was clicked');
-            changeReadStatus(myLibrary.find(({id}) => id === event.target.getAttribute('data-id')));
+            myLibrary.find(({id}) => id === event.target.getAttribute('data-id')).changeReadStatus();
             break;
         case 'remove':
             console.log('remove item was clicked');
-            removeBook(myLibrary.find(({id}) => id === event.target.getAttribute('data-id')));
+            myLibrary.find(({id}) => id === event.target.getAttribute('data-id')).removeBook();
             break;
     }
 });
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (bookForm.checkValidity()) {
+        bookDialog.close();
+
+        const titleValue = titleInput.value;
+        const authorValue = authorInput.value;
+        const pagesValue = Number(pagesInput.value);
+        const readValue = readInput.checked;
+
+        bookForm.reset();
+        
+        addBookToLibrary(titleValue, authorValue, pagesValue, readValue);
+        updateLibraryDisplay();
+    }
+});
+
+addBookToLibrary("I Spy", "Jean Marzollo", 40, true);
+addBookToLibrary("The Name Of This Book Is Secret", "Pseudonymous Bosch", 400, true);
+addBookToLibrary("Of Mice and Men", "John Steinbeck", 144, false);
+addBookToLibrary("Brave New World", "Aldous Huxley", 326, true);
+addBookToLibrary("The Crucible", "Arthur Miller", 61, true);
+addBookToLibrary("Do Androids Dream Of Electric Sheep?", "Philip K. Dick", 258, false)
+
+updateLibraryDisplay();
